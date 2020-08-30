3d967e714e133b74a712857020fdd5bedf99069a
devpedrohro <devpedrohro@gmail.com>
2020-08-30 00:24:13 -0300

    create project and dart files

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:covid_19/constant.dart';
+import 'package:covid_19/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Covid 19",
+      theme: ThemeData(
+          scaffoldBackgroundColor: kBackgroundColor,
+          fontFamily: "Poppins",
+          textTheme: TextTheme(body1: TextStyle(color: kBodyTextColor))),
+      home: HomeScreen(),
+    );
+  }
+}
